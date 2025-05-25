85b92095bab802b598ee9ff9c02a42efc215f60d
Satya Satyajit <satyajitsahoo28252@gmail.com>
2025-05-25 15:44:41 +0530

    loggers

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,12 +1,20 @@
 const express = require("express") ;
+const { log } = require("node:console");
 const app = express() ;
 
 //app.use (middleware) 
-app.use((req,res,next) =>{
-    console.log("I am a middleware");
-    next();
-}) 
 
+// app.use((req,res,next) =>{
+//     console.log("I am a middleware");
+//     next();
+// }) 
+
+
+app.use((req,res,next) =>{
+ req.time = new Date(Date.now()).toString() ;
+ console.log(req.method, req.hostname , req.path , req.time);
+ next() ;
+})
 
 app.get("/" ,(req,res) => {
     res.send("Hi ! I am root") ;
