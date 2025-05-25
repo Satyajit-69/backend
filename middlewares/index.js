a3a4f229513ad3d86a5a8c166d99c4933e381feb
Satya Satyajit <satyajitsahoo28252@gmail.com>
2025-05-25 15:30:04 +0530

    next function

---
+++ middlewares/index.js
@@ -0,0 +1,24 @@
+const express = require("express") ;
+const app = express() ;
+
+//app.use (middleware) 
+app.use((req,res,next) =>{
+    console.log("I am a middleware");
+    next();
+}) 
+
+
+app.get("/" ,(req,res) => {
+    res.send("Hi ! I am root") ;
+}) ;
+
+
+app.get("/random" ,(req,res) => {
+    res.send("this is a random page") ;
+})
+
+app.listen(3000,()=>{
+ console.log("server listing on port 3000");
+})
+
+
