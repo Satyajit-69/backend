ea8861a7c12e9bdf5dc5dad98dd6f2bd07ddce69
Satya Satyajit <satyajitsahoo28252@gmail.com>
2025-06-01 10:51:54 +0530

    relationships

---
+++ relationships/index.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose ;
+main()
+.then(() =>{
+    console.log("connection successful");
+})
+.catch(err =>{
+    console.log(err);
+})
+
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/relation') ;
+}
+
+const userSchema = new Schema({   
+   
+    username : String ,
+    addresses : [
+        {    _id:false ,
+            location : String ,
+            city : String,
+        },
+    ],
+});
+
+const User = mongoose.model("User",userSchema) ;
+const addUsers = async() =>{
+    let user1 = new User({
+        username :"sherlock" ,
+        addresses: [{
+      
+            location : "Parabatia" , city : "DKL"
+        },
+        {location : "23K Street" , city : "London"},
+       ]
+    })
+
+   let res =  await user1.save() ;
+   console.log(res) ;
+}
+
+const deleteAllUsers = async () => {
+    await User.deleteMany({});
+    console.log("All users deleted");
+};
+// deleteAllUsers() ;
+addUsers() ;
